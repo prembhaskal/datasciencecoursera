cd615d5b000de5d514ff830e4f13eec8c40e7369
prembhaskal <pbhaskal@gmail.com>
2014-12-16 01:55:22 +0530

    eda project 2.

---
+++ exploratory_data_analysis/project2/eda_proj2_plot1.R
@@ -0,0 +1,19 @@
+## read the data
+NEI <- readRDS(file = "summarySCC_PM25.rds")
+SCC <- readRDS(file = "Source_Classification_Code.rds")
+
+## group the total emission per year.
+emissionsByYear <- aggregate(Emissions ~ year, data = NEI, sum)
+
+# initiate a png device
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+
+## plots for each year
+plot(emissionsByYear$year, emissionsByYear$Emissions, type = "p", 
+     ylab = "total PM2.5 Emission (in tonnes)", xlab = "year",
+     pch = 19, col = "black")
+## connecting line to show the decreasing emission.
+with(emissionsByYear, lines(year, Emissions))
+
+## close the device.
+dev.off()
