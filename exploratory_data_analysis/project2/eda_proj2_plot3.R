ab6edd1c35ddc6f9cb238c4a2b04c457ef28df30
prembhaskal <pbhaskal@gmail.com>
2014-12-17 01:32:08 +0530

    further plots used ggplot2

---
+++ exploratory_data_analysis/project2/eda_proj2_plot3.R
@@ -0,0 +1,24 @@
+## ggplot 2 library
+
+library(ggplot2)
+
+## read the data
+NEI <- readRDS(file = "summarySCC_PM25.rds")
+SCC <- readRDS(file = "Source_Classification_Code.rds")
+
+
+baltimoreData <- NEI[NEI$fips == "24510", ]
+
+## group the total emission per year.
+emissionsByYearType <- aggregate(Emissions ~ year + type, data = baltimoreData, sum)
+
+# initiate a png device
+png(filename = "plot3.png", width = 800, height = 480, units = "px")
+
+g <- ggplot(emissionsByYearType, aes(x = year, y = Emissions))
+g <- g + geom_point() + geom_line() 
+g <- g + facet_grid( . ~ type)
+g <- g + labs(x = "year", y = "PM2.5 emissions (in tonnes)")
+print(g)
+
+dev.off()
