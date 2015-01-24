26aad2f0b4f65f422be65a834be2ba72b0266068
prembhaskal <pbhaskal@gmail.com>
2015-01-24 13:44:15 +0530

    project 2 ... finished r code for the document.

---
+++ reproducible_reasearch/rep_data_project2.R
@@ -0,0 +1,98 @@
+# required libraries
+  library(dplyr)
+  library(ggplot2)
+
+# load the data
+
+  con <- file("StormData.csv")
+  fileData <- readLines(con, 10)
+  close(con)
+
+  stormData <- read.csv("StormData.csv")
+  
+  summary(stormData)
+  dim(stormData)
+  
+# analysing types of events
+  length(unique(stormData$EVTYPE))
+  
+# finding fatalities causing harmful events.
+  fatalitiesVsEvtype <- aggregate(FATALITIES ~ EVTYPE, data = stormData, sum)
+  fatalitiesVsEvtype <- fatalitiesVsEvtype[order(fatalitiesVsEvtype$FATALITIES, decreasing = TRUE), ]
+  
+  # faster alternative using tapply
+#   facilitiesVsEvt <- tapply(stormData$FATALITIES, stormData$EVTYPE, sum)
+#   testDat <- data.frame("event" = names(facilitiesVsEvt), "facilities" = facilitiesVsEvt)
+#   testDat <- testDat[order(testDat$facilities, decreasing = TRUE), ]
+  
+# top events causing major fatalities
+  topEventForFatalities <- head(fatalitiesVsEvtype, 10)
+  
+  g <- ggplot(data = topEventForFatalities, aes(x = EVTYPE, y = FATALITIES))
+  g <- g + geom_bar(stat = "identity")
+  g <- g + labs(x = "event", y = "total fatalities")
+  g <- g + ggtitle("top events causing fatalities")
+  g <- g + theme(axis.text.x = element_text(angle = 90))
+  print(g)
+  
+# finding injuries causing events
+  injuriesVsEvtype <- aggregate(INJURIES ~ EVTYPE, data = stormData, sum)
+  injuriesVsEvtype <- injuriesVsEvtype[order(injuriesVsEvtype$INJURIES, decreasing = TRUE),]
+  
+# top events causing major injuries.
+  topEventForInjuries <- head(injuriesVsEvtype, 10)
+  g <- ggplot(data = topEventForInjuries, aes(x = EVTYPE, y = INJURIES))
+  g <- g + geom_bar(stat = "identity")
+  g <- g + labs(x = "event", y = "total injuries")
+  g <- g + ggtitle("top events causing injuries")
+  g <- g + theme(axis.text.x = element_text(angle = 90))
+  print(g)
+  
+# convert all to upper case
+  stormData$PROPDMGEXP <- toupper(stormData$PROPDMGEXP)
+  stormData$CROPDMGEXP <- toupper(stormData$CROPDMGEXP)
+# convert non character exponent to 1
+  stormData[!grepl("[K|M|H|B|0-9]", stormData$PROPDMGEXP), "PROPDMGEXP"] <- 0
+  stormData[!grepl("[K|M|H|B|0-9]", stormData$CROPDMGEXP), "CROPDMGEXP"] <- 0
+  
+  
+# map for character vs exponential values.
+  char <- c(0,1,2,3,4,5,6,7,8,9,"H","K","M","B")  
+  val <- c(0,1,2,3,4,5,6,7,8,9,2,3,6,9)
+  expVsVal <- data.frame(char, val)
+  
+  colnames(expVsVal) <- c("char", "propexp")
+  
+  stormData <- merge(x = stormData, y = expVsVal, by.x = "PROPDMGEXP", by.y = "char")
+  
+  colnames(expVsVal) <- c("char", "cropexp")
+  stormData <- merge(x = stormData, y = expVsVal, by.x = "CROPDMGEXP", by.y = "char")
+  
+  stormData <- mutate(stormData, totalDamage  = PROPDMG * 10 ^ propexp + CROPDMG * 10 ^ cropexp)
+  
+# verifying the data
+  head(stormData[, c("PROPDMG", "PROPDMGEXP", "CROPDMG", "CROPDMGEXP", "totalDamage")])
+  
+# group events by total damage
+  totalDamageVsEvtType <- aggregate(totalDamage ~ EVTYPE, data = stormData, sum)
+# getting the events causing maximum damage
+  totalDamageVsEvtType <- totalDamageVsEvtType[order(totalDamageVsEvtType$totalDamage, decreasing = TRUE), ]
+  
+# considering the damages which are greater than 0
+  totalDamageVsEvtType <- totalDamageVsEvtType[totalDamageVsEvtType$totalDamage > 0,]
+  
+  head(totalDamageVsEvtType, 10)
+# taking logs to fit the scale
+  totalDamageVsEvtType$totalDamage <- totalDamageVsEvtType$totalDamage/10^9
+# events causing damage
+  topEventsForDamage <- head(totalDamageVsEvtType, 10)
+  
+  g <- ggplot(data = topEventsForDamage, aes(x = EVTYPE, y = totalDamage))
+  g <- g + geom_bar(stat = "identity")
+  g <- g + labs(x = "event", y = "total damages (in Billion Dollars)")
+  g <- g + ggtitle("top events causing financial damages")
+  g <- g + theme(axis.text.x = element_text(angle = 90))
+  print(g)
+  
+  
+  
