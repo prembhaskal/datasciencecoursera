44f93184f5eddce3d4f003c5fd8276c38df583d3
prembhaskal <pbhaskal@gmail.com>
2015-02-17 02:13:51 +0530

    plots to compare variance. plots to check that the distribution approaches the normal dist...as the size of the sample grows. (CLT)
    good use of ggplot and aes and other things.

--- statistical_inference/project/stat_inf_proj1.R
+++ statistical_inference/project/stat_inf_proj1.R
@@ -28,7 +28,47 @@ g <- g + scale_color_manual(name = "",
 print(g)
 
 
+## sample variance vs theoretical variance
 
+varOfAverages <- sapply(X = sequence, function(p) {
+  samples <- rexp(n, lambda)
+  round(var(samples), 1)
+})
+
+dfVars <- data.frame("var" = varOfAverages)
+
+g <- ggplot()
+g <- g + geom_density(data = dfVars, aes(x = var, color = "sample_variance"), size = 2)
+g <- g + geom_vline(aes(xintercept = 1/lambda, size = 2, color = "theoretical_variance"))
+g <- g + labs(x = "x", y = "density")
+g <- g + ggtitle("sample variance vs theoretical variance")
+# g <- g + xlim(0, 10)
+g <- g + scale_color_manual(name = "",
+                            values = c("sample_variance" = "red", "theoretical_variance" = "blue"))
+
+print(g)
+
+## showing that the distribution is approximately normal.
+sampleAverages <- function(n, df) {
+  varOfAverages <- sapply(X = df, function(p) {
+    samples <- rexp(n, lambda)
+    round(mean(samples), 1)
+  })
+}
+
+samples <- c(sampleAverages(20, seq(1 : totalIterations)), 
+             sampleAverages(40, seq(1 : totalIterations)),
+             sampleAverages(80, seq(1 : totalIterations)))
+sampleCounts <- factor(rep(c(20, 40, 80), each = totalIterations))
+
+samplesDf <- data.frame(x = samples, size = sampleCounts)
+
+g <- ggplot(data = samplesDf, aes(x = x))
+g <- g + geom_histogram(aes(y = ..density.., fill = size), binwidth = 0.1, colour = "black")##binwidth = 0.3)
+g <- g + geom_vline(xintercept = 1/lambda)
+g <- g + geom_density(size = 2, data = samplesDf, aes(x = x))
+g <- g + facet_grid(. ~ size)
+print(g)
 ## the plot shows that the mean of the distribution is around 5, which is the same as the 
 # sample mean.
 
