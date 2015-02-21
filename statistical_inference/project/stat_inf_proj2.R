d9550f46d863f4278b6828641110dcaca5ba0c34
prembhaskal <pbhaskal@gmail.com>
2015-02-22 01:59:49 +0530

    doing some hypothesis testing on the data. (same dose, different supplements).

--- statistical_inference/project/stat_inf_proj2.R
+++ statistical_inference/project/stat_inf_proj2.R
@@ -32,5 +32,38 @@ g <- g + ggtitle("bar plot of average tooth growth vs dose grouped by different 
 print(g)
 
 
+## doing t tests for different supplements taken with same dose.
+supp1dose1 <- ToothGrowth[ToothGrowth$supp == "OJ" & ToothGrowth$dose == "0.5",] 
+supp2dose1 <- ToothGrowth[ToothGrowth$supp == "VC" & ToothGrowth$dose == "0.5",] 
+
+## since we don't have any information of variance of different group, let us assume it is different.
+t.test(supp1dose1$len, supp2dose1$len, paired = FALSE,  var.equal = FALSE)
+
+## DOUBT what is the meaning or interpretation of the values, conf interval obtained by doing the "t-tests".
+supp1dose2 <- ToothGrowth[ToothGrowth$supp == "OJ" & ToothGrowth$dose == "1",] 
+supp2dose2 <- ToothGrowth[ToothGrowth$supp == "VC" & ToothGrowth$dose == "1",] 
+
+## since we don't have any information of variance of different group, let us assume it is different.
+t.test(supp1dose2$len, supp2dose2$len, paired = FALSE,  var.equal = FALSE)
+
+# supp1dose3 <- ToothGrowth[ToothGrowth$supp == "OJ" & ToothGrowth$dose == "2",] 
+# supp2dose3 <- ToothGrowth[ToothGrowth$supp == "VC" & ToothGrowth$dose == "2",] 
+# 
+# ## since we don't have any information of variance of different group, let us assume it is different.
+# t.test(supp1dose3$len, supp2dose3$len, paired = FALSE,  var.equal = FALSE)
+
+
+## doing a two group hypothesis testing assuming that mean of the tooth growth is same ( that is 
+## different in mean is 0 -- NULL HYPOTHESIS)
+
+dose1Data <- ToothGrowth[ToothGrowth$dose == "2",]
+t.test(len ~ supp, paired = FALSE, var.equal = FALSE, data = dose1Data)
+
+## in our case, n = 10, therefore df = 9, so 
+Z_1minusAlpha  = qt(0.975, df = 9)
+
+## since the absolute test Statistic is lesser than Z_1minusAlpha, we fail to reject null hypothesis, so 
+## the 2 data groups have same mean.
+
 ## clear figure
 dev.off()
