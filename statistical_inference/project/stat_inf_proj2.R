8668d322913763eb683c9ef15f6a15e8482eb421
prembhaskal <pbhaskal@gmail.com>
2015-02-25 00:51:56 +0530

    completed the project...not satisfactory though...people did very good.

--- statistical_inference/project/stat_inf_proj2.R
+++ statistical_inference/project/stat_inf_proj2.R
@@ -15,7 +15,8 @@ nrow(ToothGrowth)
 unique(ToothGrowth$supp)
 class(ToothGrowth$supp)
 
-unique(paste(ToothGrowth$supp,ToothGrowth$dose))
+# unique(paste(ToothGrowth$supp,ToothGrowth$dose))
+unique(ToothGrowth[,c("supp", "dose")])
 
 ## the data is about the tooth growth as a affect of 2 supplements VC and OJ, with different doses of 0.5, 1.0 and 2.0
 
@@ -52,18 +53,34 @@ t.test(supp1dose2$len, supp2dose2$len, paired = FALSE,  var.equal = FALSE)
 # ## since we don't have any information of variance of different group, let us assume it is different.
 # t.test(supp1dose3$len, supp2dose3$len, paired = FALSE,  var.equal = FALSE)
 
+## T Hypothesis tests and CI tests
+## doing a two group hypothesis testing assuming the NULL HYPOTHESIS that mean of the distribution of tooth growth is same 
+## on use of the two supplements (VC and OJ).
 
-## doing a two group hypothesis testing assuming that mean of the tooth growth is same ( that is 
-## different in mean is 0 -- NULL HYPOTHESIS)
-
-dose1Data <- ToothGrowth[ToothGrowth$dose == "2",]
-t.test(len ~ supp, paired = FALSE, var.equal = FALSE, data = dose1Data)
-
+## Z (97.5th quantile)
 ## in our case, n = 10, therefore df = 9, so 
 Z_1minusAlpha  = qt(0.975, df = 9)
+Z_1minusAlpha
 
-## since the absolute test Statistic is lesser than Z_1minusAlpha, we fail to reject null hypothesis, so 
+## testing supplement dose of 0.5`
+dose1Data <- ToothGrowth[ToothGrowth$dose == 0.5,]
+t.test(len ~ supp, paired = FALSE, var.equal = FALSE, data = dose1Data)
+
+## Result, here we see that the test statistic (3.1) is greater than the 97.5 quantile (Z(1-alpha)) of 2.26. so we 
+## reject the NULL hypothesis.
+
+## testing supplement dose of 1.0
+dose2Data <- ToothGrowth[ToothGrowth$dose == 1.0,]
+t.test(len ~ supp, paired = FALSE, var.equal = FALSE, data = dose2Data)
+
+## since test_statistic (4.03) is greater than the Z_quantile (97.5%) of 2.26, so we 
+## reject the NULL hypothesis.
+
+dose3Data <- ToothGrowth[ToothGrowth$dose == 2,]
+t.test(len ~ supp, paired = FALSE, var.equal = FALSE, data = dose1Data)
+
+## since the absolute test Statistic is lesser than Z_1minusAlpha, we fail to reject the NULL hypothesis, so 
 ## the 2 data groups have same mean.
 
 ## clear figure
-dev.off()
+# dev.off()
